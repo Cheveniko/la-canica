364ea4a6b4c9fc162a8edf1b66bb8082b5a3146d
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2023-09-19 02:32:58 -0500

    API: add check to filter hidden articles

--- src/utils/get-articles.js
+++ src/utils/get-articles.js
@@ -5,20 +5,23 @@ import { cache } from "react";
 
 export const getHeroArticle = cache(async () => {
   await connectDB();
-  const heroArticle = await Article.findOne({ kind: "main" });
+  const heroArticle = await Article.findOne({ kind: "main", hidden: false });
   return heroArticle;
 });
 
 export const getSidebarArticles = cache(async () => {
   await connectDB();
-  const sidebarArticles = await Article.find({ kind: "trending" });
+  const sidebarArticles = await Article.find({
+    kind: "trending",
+    hidden: false,
+  });
   const slicedSidebarArticles = sidebarArticles.slice(-2).reverse();
   return slicedSidebarArticles;
 });
 
 export const getNewsArticles = cache(async () => {
   await connectDB();
-  const newsArticles = await Article.find({ kind: "news" });
+  const newsArticles = await Article.find({ kind: "news", hidden: false });
   const slicednewsArticles = newsArticles.reverse();
   return slicednewsArticles;
 });
