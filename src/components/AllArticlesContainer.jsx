db414acd3c706f9afe6165ddcf25a922f807d295
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2023-10-14 04:48:58 -0500

    Style: change colors

--- src/components/AllArticlesContainer.jsx
+++ src/components/AllArticlesContainer.jsx
@@ -1,13 +1,14 @@
-import { getAllArticles } from "@/utils/get-articles";
 import ArticleCard from "./ArticleCard";
 import EditButton from "./buttons/EditButton";
 import DeleteButton from "./buttons/DeleteButton";
+
+import { getAllArticles } from "@/utils/get-articles";
 
 async function AllArticlesContainer() {
   const allArticles = await getAllArticles();
 
   return (
-    <div className="container mx-auto px-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6">
+    <div className="container mx-auto px-4 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6">
       {allArticles.map((article) => (
         <ArticleCard
           key={article._id}
@@ -16,7 +17,7 @@ async function AllArticlesContainer() {
           bannerHeigth={675}
           type="admin"
         >
-          <div className="flex justify-between mt-4 bg-gradient-to-r from-amber-400 to-red-600 rounded-full">
+          <div className="flex justify-between mt-4 border border-cyan-400 border-opacity-50 rounded-lg">
             <EditButton />
             <DeleteButton />
           </div>
