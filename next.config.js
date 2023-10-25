f756f5352a3088eaaf1af800ce5234234fc45302
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2023-10-25 04:49:46 -0500

    add CORS allowed domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.lacanica.ec",
+          }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,POST,PUT",
+          },
+        ],
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
