425c52173b2c284e7042b75e51d99318665c6ffa
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2023-11-02 04:36:43 -0500

    - Add new root layout for login and site

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -3,20 +3,17 @@ import type { Metadata } from "next";
 import Radnika from "next/font/local";
 
 import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
 
-import "../styles/globals.css";
+import "../../styles/globals.css";
 
 const radnika = Radnika({
-  src: "../../public/fonts/Radnika-Medium.otf",
+  src: "../../../public/fonts/Radnika-Medium.otf",
   display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: {
-    template: "%s | La Canica",
-    default: "La Canica",
-  },
+  title: "Iniciar sesión | La Canica",
+
   description: "Noticias tan falsas que en Ecuador parecen reales 🇪🇨",
   keywords: [
     "Canica",
@@ -38,9 +35,8 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={radnika.className}>
-        <Navbar />
-        <main className="bg-cyan-950 py-4">{children}</main>
-        <Footer />
+        <Navbar variant="transparent" />
+        {children}
       </body>
     </html>
   );
---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+
+import Radnika from "next/font/local";
+
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+
+import "../../styles/globals.css";
+
+const radnika = Radnika({
+  src: "../../../public/fonts/Radnika-Medium.otf",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | La Canica",
+    default: "La Canica",
+  },
+  description: "Noticias tan falsas que en Ecuador parecen reales 🇪🇨",
+  keywords: [
+    "Canica",
+    "Ecuador",
+    "Noticias",
+    "Quito",
+    "Guayaquil",
+    "Cuenca",
+    "Politica",
+  ],
+  referrer: "origin-when-cross-origin",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body className={radnika.className}>
+        <Navbar variant="color" />
+        <main className="bg-cyan-950 py-4">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
