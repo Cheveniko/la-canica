7b89d4212ce0427488d79a1d4e2eb74a8dbea53c
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2023-11-02 04:37:21 -0500

    - Login Card

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -1,29 +1,60 @@
+"use client";
 import { FC } from "react";
 
 import Image from "next/image";
 
-import LoginItem from "./LoginItem";
+import { signIn } from "next-auth/react";
+
+import { motion } from "framer-motion";
+
 import FooterLogo from "../../../public/img/bola.png";
 import TextLogo from "../../../public/img/header_logo.png";
-
-const signInProviders = [
-  { name: "google", style: "border border-[#747775] text-[#1F1F1F] bg-white" },
-  { name: "facebook", style: "bg-[#006aff]" },
-  { name: "email", style: "" },
-];
+import GoogleIcon from "../../../public/svg/google.svg";
+import { FaFacebook, FaEnvelope } from "react-icons/fa6";
 
 const LoginCard: FC = () => {
   return (
-    <div className="container w-1/3 rounded-lg border border-white px-8 py-4">
-      <Image
-        src={FooterLogo}
-        alt="Logo de la Canica"
-        className="mx-auto h-auto w-[120px]"
-      />
-      <h1 className="my-8 text-center text-4xl">Iniciar sesión</h1>
-      {signInProviders.map((provider, index) => (
-        <LoginItem key={index} provider={provider} />
-      ))}
+    //Alternativa de fondo bg-zinc-300 text-gray-800
+    <div className="container w-5/6 rounded-xl border border-black bg-slate-300 px-0 py-10 shadow-md shadow-black md:w-fit md:px-14">
+      <motion.div
+        transition={{
+          duration: 1,
+          type: "spring",
+          damping: 25,
+          stiffness: 100,
+        }}
+        whileHover={{ rotate: 360 }}
+      >
+        <Image
+          src={FooterLogo}
+          alt="Logo de la Canica"
+          className="mx-auto h-auto w-[120px] rounded-full border border-black"
+        />
+      </motion.div>
+      <h1 className="my-8 text-center text-4xl text-black">Iniciar sesión</h1>
+      <div className="flex flex-col gap-y-4">
+        <button
+          onClick={() => signIn("google")}
+          className="mx-auto flex h-11 w-5/6 items-center rounded-md border border-[#747775] bg-white px-3 text-center text-base text-[#1F1F1F] hover:bg-neutral-200 sm:text-lg md:w-80"
+        >
+          <Image src={GoogleIcon} alt="Botón de iniciar sesión con Google" />
+          <p className="ps-3">Continuar con Google</p>
+        </button>
+        <button
+          onClick={() => signIn("facebook")}
+          className="mx-auto flex h-11 w-5/6 items-center rounded-md border border-blue-400 bg-[#006aff] px-3 text-center text-base text-white hover:bg-blue-700 sm:text-lg md:w-80"
+        >
+          <FaFacebook className="h-5 w-5" />
+          <p className="ps-3">Continuar con Facebook</p>
+        </button>
+        <button
+          onClick={() => signIn("email")}
+          className="mx-auto flex h-11 w-5/6 items-center rounded-md border border-black bg-amber-400 px-3 text-center text-base text-neutral-950 hover:bg-amber-500 sm:text-lg md:w-80"
+        >
+          <FaEnvelope className="h-5 w-5" />
+          <p className="ps-3">Continuar con Email</p>
+        </button>
+      </div>
     </div>
   );
 };
